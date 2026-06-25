c4e698469b0e5d793db32a880e7dfb554c86ea25
Zisan <mdzisanulhaque989@gmail.com>
2026-06-25 22:44:41 +0600

    Setting Up Route, Controller, Service For User Registration API

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,29 @@
 import express, { Application, Request, Response } from "express";
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import config from "./config";
+import httpStatus from "http-status";
+import { prisma } from "./lib/prisma";
+import bcrypt from "bcryptjs";
+import { userRoutes } from "./module/user/user.route";
 
+const app: Application = express();
 
-const app : Application = express(); 
-
-app.use(cors({
-    origin : config.app_url,
-    credentials : true,
-}))
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get("/",(req:Request, res: Response) =>{
-    res.send("Hello World")
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.use("/api/users", userRoutes)
+
 export default app;
