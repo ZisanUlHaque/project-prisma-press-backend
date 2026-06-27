6af92bada2b1644fe4e9d3b0ada98019e4ab4396
Zisan <mdzisanulhaque989@gmail.com>
2026-06-27 22:49:14 +0600

    Implementing Jwt For Access Token And Refresh Token

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,23 +1,50 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
-import { iLoginUser } from "./auth.interface"
+import { iLoginUser } from "./auth.interface";
+import jwt, { SignOptions } from "jsonwebtoken";
+import config from "../../config";
+import { jwtUtils } from "../../utils/jwt";
 
-const loginUser = async(payload : iLoginUser) =>{
-    const {email,password} = payload;
+const loginUser = async (payload: iLoginUser) => {
+  const { email, password } = payload;
 
-    const user = await prisma.user.findUniqueOrThrow({
-        where : {email}
-    })
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { email },
+  });
 
-    const isPasswordMatched = await bcrypt.compare(password,user.password);
+  const isPasswordMatched = await bcrypt.compare(password, user.password);
 
-    if(!isPasswordMatched){
-        throw new Error("Password is incorrect");
-    }
+  if (!isPasswordMatched) {
+    throw new Error("Password is incorrect");
+  }
 
-    return user;
-}
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  const accessToken = jwt.sign({ jwtPayload }, config.jwt_access_secret, {
+    expiresIn: config.jwt_access_expires_in,
+  } as SignOptions);
+
+  //   const refreshToken = jwt.sign({ jwtPayload }, config.jwt_refresh_secret, {
+  //     expiresIn: config.jwt_refresh_expires_in,
+  //   } as SignOptions);
+
+  const refreshToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_refresh_secret,
+    config.jwt_refresh_expires_in as SignOptions,
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+  };
+};
 
 export const authService = {
-    loginUser
-}
+  loginUser,
+};
