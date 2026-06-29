413253155daba511bce8e9c458286ab75dc75712
Zisan <mdzisanulhaque989@gmail.com>
2026-06-29 13:30:53 +0600

    Implementing Update Post And Delete Post APIs

--- src/module/posts/post.controller.ts
+++ src/module/posts/post.controller.ts
@@ -25,7 +25,7 @@ const getAllPosts = catchAsync(async(req : Request, res: Response, next : NextFu
 
       sendResponse(res , {
       success : true,
-      statusCode : httpStatus.CREATED,
+      statusCode : httpStatus.OK,
       message : "Post retrieved successfully",
       data : result
     })
@@ -36,19 +36,72 @@ const getPostsStats = catchAsync(async(req : Request, res: Response, next : Next
 })
 
 const getMyPosts = catchAsync(async(req : Request, res: Response, next : NextFunction) =>{
-    
+    const authorId = req.user?.id
+
+    const result = await postService.getMyPosts(authorId as string)
+
+      sendResponse(res , {
+      success : true,
+      statusCode : httpStatus.OK,
+      message : "My Post retrieved successfully",
+      data : result
+    })
 })
 
 const getPostById = catchAsync(async(req : Request, res: Response, next : NextFunction) =>{
-    
+    const postId = req.params.postId;
+
+    if (!postId) {
+      throw new Error("Post id required in params")
+    }
+
+    const result = await postService.getPostById(postId as string)
+
+      sendResponse(res , {
+      success : true,
+      statusCode : httpStatus.OK,
+      message : "Post retrieved successfully",
+      data : result
+    })
 })
 
-
 const updatePost = catchAsync(async(req : Request, res: Response, next : NextFunction) =>{
+    const authorId = req.user?.id
+    const isAdmin = req.user?.role === "ADMIN";
+
+    const postId = req.params.postId
+    const payload = req.body
+
+    const result = await postService.updatePost(postId  as string,payload,authorId as string, isAdmin)
+
+      sendResponse(res , {
+      success : true,
+      statusCode : httpStatus.OK,
+      message : "Post updated successfully",
+      data : result
+    })
     
 })
 
 const deletePost = catchAsync(async(req : Request, res: Response, next : NextFunction) =>{
+    const authorId = req.user?.id
+    const isAdmin = req.user?.role === "ADMIN";
+
+    const postId = req.params.postId
+   if (!postId) {
+     throw new Error("Post id required in params")
+    }
+
+    const payload = req.body
+
+     await postService.deletePost(postId  as string,authorId as string, isAdmin)
+
+      sendResponse(res , {
+      success : true,
+      statusCode : httpStatus.OK,
+      message : "Post deleted successfully",
+      data : null
+    })
     
 })
 
--- src/module/posts/post.service.ts
+++ src/module/posts/post.service.ts
@@ -1,52 +1,151 @@
-import { prisma } from "../../lib/prisma"
-import { IcreatePostPayload } from "./post.interface"
+import { prisma } from "../../lib/prisma";
+import { IcreatePostPayload, IUpdatePostPayload } from "./post.interface";
 
-const createPost = async(payload:IcreatePostPayload,userId : string) =>{
-    const result = await prisma.post.create({
-        data : {
-            ...payload,
-            authorId : userId
-        }
-    })
+const createPost = async (payload: IcreatePostPayload, userId: string) => {
+  const result = await prisma.post.create({
+    data: {
+      ...payload,
+      authorId: userId,
+    },
+  });
 
-    return result
-}
-const getAllPosts = async() =>{
-    const posts = await prisma.post.findMany({
-        include :{
-            author : {
-                omit : {
-                    password : true
-                }
-            },
-            comments : true
-        } 
-    });
+  return result;
+};
+const getAllPosts = async () => {
+  const posts = await prisma.post.findMany({
+    include: {
+      author: {
+        omit: {
+          password: true,
+        },
+      },
+      comments: true,
+    },
+  });
 
-    return posts
-}
-const getPostsStats = () =>{
+  return posts;
+};
+const getPostsStats = () => {};
 
-}
-const getMyPosts = () =>{
+const getMyPosts = async (authorId: string) => {
+  const result = await prisma.post.findMany({
+    where: {
+      authorId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      comments: true,
+      author: {
+        omit: {
+          password: true,
+        },
+      },
+      _count: {
+        select: {
+          comments: true,
+        },
+      },
+    },
+  });
 
-}
-const getPostById = () =>{
+  return result;
+};
 
-}
-const updatePost = () =>{
+const getPostById = async (postId: string) => {
+  const post = await prisma.post.findUniqueOrThrow({
+    where: {
+      id: postId,
+    },
+  });
 
-}
-const deletePost = () =>{
+  const updatedPost = await prisma.post.update({
+    where: {
+      id: postId,
+    },
+    data: {
+      views: {
+        increment: 1,
+      },
+    },
+    include: {
+      author: {
+        omit: {
+          password: true,
+        },
+      },
+      comments: true,
+    },
+  });
 
-}
+  return updatedPost;
+};
+
+const updatePost = async (
+  postId: string,
+  payload: IUpdatePostPayload,
+  authorId: string,
+  isAdmin: boolean,
+) => {
+  const post = await prisma.post.findUniqueOrThrow({
+    where: {
+      id: postId,
+    },
+  });
+
+  if (!isAdmin && post.authorId !== authorId) {
+    throw new Error("You are not owner of this post");
+  }
+
+  const result = await prisma.post.update({
+    where: {
+      id: postId,
+    },
+    data: payload,
+    include: {
+      author: {
+        omit: {
+          password: true,
+        },
+      },
+      comments: true,
+    },
+  });
+
+  return result;
+};
+
+const deletePost = async (
+  postId: string,
+  authorId: string,
+  isAdmin: boolean,
+) => {
+  const post = await prisma.post.findUniqueOrThrow({
+    where: {
+      id: postId,
+    },
+  });
+
+  if (!isAdmin && post.authorId !== authorId) {
+    throw new Error("You are not owner of this post");
+  }
+
+  const result = await prisma.post.delete({
+    where: {
+      id: postId,
+    },
+  });
+
+  return null;
+};
 
 export const postService = {
-    createPost,
-    getAllPosts,
-    getPostsStats,
-    getMyPosts,
-    getPostById,
-    updatePost,
-    deletePost
-}
+  createPost,
+  getAllPosts,
+  getPostsStats,
+  getMyPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+};
